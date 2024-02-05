a52e926369e8963cf3e5617d0200756b1e146697
Laetitia <laetitia.teodorescu@gmail.com>
2024-02-05 16:13:55 +0100

    Preference comparison experiments

--- quality_metrics/gpt_judgements/judgements.py
+++ quality_metrics/gpt_judgements/judgements.py
@@ -1,6 +1,49 @@
+import os
+import re
+import json
 from typing import List
+from pprint import pprint
 
-from quality_metrics.gpt_judgements.prompts import five_fold_ranking_prompt
+import string
+import random
+
+import numpy as np
+from openai import OpenAI
+
+from openelm.mutation_model import get_completion, get_multiple_completions
+
+from quality_metrics.utils import make_puzzle, make_solution
+from quality_metrics.gpt_judgements.prompts import (
+    five_fold_ranking_prompt,
+    random_permutation_prompt,
+    five_fold_ranking_prompt_scrambled_cot
+) 
+
+
+TRAIN_PUZZLES = json.load(open('puzzles_train.json', 'r'))
+PREFERENCE_PUZZLES = json.load(open('puzzles_with_prefs.json', 'r'))
+
+
+example_template = """Puzzle and solution {i}:
+```python
+{puzzle}
+
+{solution}
+```"""
+
+example_template_docstring_before = """Puzzle and solution {i}:
+```python
+Problem description: {docstring}
+
+{puzzle}
+
+{solution}
+```"""
+
+
+random_puzzle_labels = [
+    'fjnw', 'pfmr', 'qpfc', 'wphv', 'kqsr'
+]
 
 
 def build_prompt(puzzle_strs: List[str]):
@@ -10,5 +53,210 @@ def build_prompt(puzzle_strs: List[str]):
     return five_fold_ranking_prompt.format(examples=puzzle_text)
 
 
-def get_five_fold_results(puzzle_strs):
-    pass
+def get_example_string(doctstring_mode='in_puzzle', mode='normal', puzzle_set='first_five'):
+    # docstring in {'in_puzzle', 'before_puzzle', 'none'} 
+    ex_string = ''
+    examples = []
+    sampled_puzzles = []
+    match puzzle_set:
+        case 'first_five':
+            for i in range(5):
+                sampled_puzzles.append(TRAIN_PUZZLES[i])
+            ids = list(range(5))
+        case 'preferences':
+            ids = np.random.choice(
+                list(range(len(PREFERENCE_PUZZLES))),
+                5,
+                replace=False
+            ).tolist()
+            sampled_puzzles = [PREFERENCE_PUZZLES[i] for i in ids]
+    
+    # record permutation
+    sampled_puzzles = list(enumerate(sampled_puzzles))
+    sampled_puzzles = np.random.permutation(sampled_puzzles).tolist()
+    permutation = [i for i, _ in sampled_puzzles]
+    sampled_puzzles = [p for _, p in sampled_puzzles]
+
+    if mode == 'scrambled':
+        str_ids = []
+        for _ in range(5):
+            # generate a random seq of letters: 
+            str_id = ''.join([random.choice(string.ascii_letters) for _ in range(4)])
+            str_ids.append(str_id)
+    else:
+        str_ids = None
+
+    for i, p in enumerate(sampled_puzzles):
+
+        if mode == 'normal':
+            I = i
+        else: 
+            I = str_ids[i]
+
+        match doctstring_mode:
+            case 'in_puzzle':
+                puzzle = make_puzzle(p, include_docstring=True)
+                solution = make_solution(p)
+                examples.append(example_template.format(i=I, puzzle=puzzle, solution=solution))
+            case 'before_puzzle':
+                puzzle = make_puzzle(p, include_docstring=False)
+                solution = make_solution(p)
+                doctstring = p['sol_docstring']
+                examples.append(example_template_docstring_before.format(
+                    i=I, puzzle=puzzle, solution=solution, docstring=doctstring.strip()))
+            case _:
+                puzzle = make_puzzle(p, include_docstring=False)
+                solution = make_solution(p)
+                examples.append(example_template.format(i=I, puzzle=puzzle, solution=solution))
+
+    return '\n\n'.join(examples), permutation, str_ids, ids
+
+
+def get_five_fold_results(client, cfg_generation, docstring_mode='in_puzzle', mode='normal',
+                          puzzle_set='first_five'):
+
+    match mode:
+        case 'normal':
+            prompt_template = five_fold_ranking_prompt
+        case 'scrambled':
+            prompt_template = five_fold_ranking_prompt_scrambled_cot
+
+    # sample puzzles
+    example_str, permutation, str_ids, ids = get_example_string(docstring_mode, mode=mode, 
+                                                                puzzle_set=puzzle_set)
+    prompt = prompt_template.format(examples=example_str)
+    completion = get_completion(client, prompt, cfg_generation)
+    return completion, permutation, str_ids, ids
+
+
+def get_default_config():
+    key = os.environ['OPENAI_API_KEY']
+    client = OpenAI(api_key=key, max_retries=5, timeout=50)
+
+    cfg_generation={"temperature": 0.0,
+            "top_p": 1,
+            # TODO: rename config option?
+            "model": "gpt-3.5-turbo",
+            "max_tokens": 1000,
+    }
+    
+    return client, cfg_generation
+
+
+def test_validity(N=5, mode='normal', puzzle_set='first_five'):
+    # Tests to see whether the model produces parsable lists, and consistent outputs
+    client, cfg_generation = get_default_config()
+    list_outputs = []
+    justifications = []
+    permutations = []
+
+    if puzzle_set == 'preferences':  # to recover which puzzle we sampled
+        puz_ids = []
+
+    for i in range(N):
+        print(f'Completion {i}:')
+        completion, permutation, str_ids, ids = get_five_fold_results(
+            client, cfg_generation, docstring_mode='before_puzzle', mode=mode, 
+            puzzle_set=puzzle_set)
+        m = re.match('(.*)(\[.*\]).*', completion, re.DOTALL)
+        if m is None:
+            print('No match for list')
+            print(f'Completion is: {completion}')
+            continue
+        else:
+            string_list = m[2]
+            comment = m[1]
+
+        try:
+            result = eval(string_list)
+        except Exception as e:
+            print("Evaluation failed with exception:")
+            print(e)
+            continue
+
+        if not isinstance(result, list):
+            print(f"The result doesn't have the required type (it's a {type(result)})")
+        else:
+
+            if mode == 'scrambled':
+                # try to parse the results back into a list of indices.
+                res = []
+                try:
+                    for el in result:
+                        idx = str_ids.index(el)
+                        res.append(idx)
+                except Exception as e:
+                    print(f'Couldn\'t find "{el} in {str_ids}"')
+                    print(e)
+                    continue
+                result = res
+
+            list_outputs.append((i, result))
+            if comment:
+                justifications.append((i, comment))
+            permutations.append((i, permutation))
+
+            if puzzle_set == 'preferences':
+                puz_ids.append(ids)
+                
+
+    unique = []
+    for _, el in list_outputs:
+        if el not in unique:
+            unique.append(el)
+    print(f'Number of different elements: {len(unique)}')
+    pprint(list_outputs)
+
+    data = dict(unique=unique, list_outputs=list_outputs, justifications=justifications, 
+                permutations=permutations)
+
+    save_name = 'save_results_completion_permutations_{len}'
+    if mode == 'scrambled':
+        save_name += '_scrambled'    
+
+    with open(f'quality_metrics/gpt_judgements/{save_name}.json', 'w') as f:
+        json.dump(data, f)
+
+
+def test_permutations(N=100):
+    client, cfg_generation = get_default_config()
+    list_outputs = []
+
+    for i in range(N):
+        print(f'Step {i}')
+        completion = get_completion(client, random_permutation_prompt, cfg_generation)
+        m = re.match('.*(\[.*\])(.*)', completion)
+        if m is None:
+            print('No match for list')
+            print(f'Completion is: {completion}')
+            continue
+        else:
+            string_list = m[1]
+
+        try:
+            result = eval(string_list)
+        except Exception as e:
+            print("Evaluation failed with exception:")
+            print(e)
+            continue
+
+        if not isinstance(result, list):
+            print(f"The result doesn't have the required type (it's a {type(result)})")
+            continue
+        
+        if len(result) != 5:
+            print(f"The result doesn't have the required length ({len(result)})")
+            continue
+        
+        list_outputs.append((i, result))
+    
+    with open('quality_metrics/gpt_judgements/chatgpt_rng.json', 'w') as f:
+        json.dump(list_outputs, f)
+
+
+# perform tests
+if __name__ == "__main__":
+    client, cfg_generation = get_default_config()
+    # print(get_five_fold_results(client, cfg_generation))
+    test_validity(1000, mode='scrambled', puzzle_set='preferences')
+    # test_permutations(1000)
--- quality_metrics/gpt_judgements/prompts.py
+++ quality_metrics/gpt_judgements/prompts.py
@@ -1,23 +1,117 @@
-five_fold_ranking_prompt = """You are a helpful assistant to a teacher teaching an unergraduate programming course in Python. The teacher is proposing 5 exercises in the form of programming puzzles, and your role is to rank these puzzles from the most appropriate for the course to the least appropriate.
+five_fold_ranking_prompt = """You are a helpful assistant to a teacher teaching a second-year undergraduate programming course in Python. The teacher is proposing 5 exercises in the form of programming puzzles, and your role is to rank these puzzles from the most appropriate for the course to the least appropriate.
 
-A Python programming puzzle is defined by two functions, the *puzzle* f(...) and the *solution* g(...). f defines an algorithmic challenge, and g solves this challenge. g is a solution to f if and only if `f(g()) == True`. There can be additional optional keyword arguments to both f and g; these arguments if they exist are the same for both functions. {optional: There is a description of the operations that should be performed by the solution in the description of the puzzle.}
+A Python programming puzzle is defined by two functions, the *puzzle* f(...) and the *solution* g(...). f defines an algorithmic challenge, and g solves this challenge. g is a solution to f if and only if `f(g()) == True`. There can be additional optional keyword arguments to both f and g; these arguments, if they exist, are the same for both functions.
 
-Puzzle Format:
-Problem description: ...
+Format of the description, puzzle and solution:
 ```python
+Problem description: ...
+
 def f(solution, args=...) -> bool:
     # Python code to test the solution returned by g.
-    # This function is a test unit and must return True if the solution is correct, False otherwise.
+    # This function behaves like a unit test and must return True if the solution is correct, False otherwise.
 
 def g(args=...) -> solution:
     # Python code to generate a solution for the problem.
     # The solution should generalize to all possible args.
     return solution
+```
 
-assert f(g()) == True
-
-You are classifying puzzles for a second year undergraduate Python course. Puzzles and their solutions should be ranked highly if they provide important opportunities for learning for students of that level (both by searching for the solution and looking at the provided solution, so you should look at the quality of both f and g). Clarity is important. You will be presented with 5 puzzles and their solutions, numbered from 0 to 4, and you should respond by giving your ranking as a list of indices, from the most relevant to least relevant, like so: Answer: [...]
+You are ranking puzzles for a second year undergraduate Python course. Puzzles and their solutions should be ranked highly if they provide important opportunities for learning for students of that level (both by searching for the solution and looking at the provided solution, so you should look at the quality of both f and g). Clarity is important. You will be presented with 5 puzzles and their solutions, numbered from 0 to 4, and you should respond by giving your ranking as a list of indices, from the most relevant to least relevant, like so: Answer: [...]
 
 {examples}
 
 Answer:"""
+
+random_permutation_prompt = """Can you give me a random permutation of the list [0, 1, 2, 3, 4] please? Format this as a list of 5 elements from 0 to 4.
+
+Answer:
+"""
+
+five_fold_ranking_prompt_scrambled = """You are a helpful assistant to a teacher teaching a second-year undergraduate programming course in Python. The teacher is proposing 5 exercises in the form of programming puzzles, and your role is to rank these puzzles from the most appropriate for the course to the least appropriate.
+
+A Python programming puzzle is defined by two functions, the *puzzle* f(...) and the *solution* g(...). f defines an algorithmic challenge, and g solves this challenge. g is a solution to f if and only if `f(g()) == True`. There can be additional optional keyword arguments to both f and g; these arguments, if they exist, are the same for both functions.
+
+Format of the description, puzzle and solution:
+```python
+Problem description: ...
+
+def f(solution, args=...) -> bool:
+    # Python code to test the solution returned by g.
+    # This function behaves like a unit test and must return True if the solution is correct, False otherwise.
+
+def g(args=...) -> solution:
+    # Python code to generate a solution for the problem.
+    # The solution should generalize to all possible args.
+    return solution
+```
+
+You are ranking puzzles for a second year undergraduate Python course. Puzzles and their solutions should be ranked highly if they provide important opportunities for learning for students of that level (both by searching for the solution and looking at the provided solution, so you should look at the quality of both f and g). Clarity is important. You will be presented with 5 puzzles and their solutions, with random 4-letter ids. You should respond by giving your ranking as a list of ids, from the most relevant to least relevant. The ids should be inside double quotes, like so: Answer: ["aaaa", "bbbb", ...]
+
+{examples}
+
+Answer:"""
+
+
+five_fold_ranking_prompt_scrambled = """You are a helpful assistant to a teacher teaching a second-year undergraduate programming course in Python. The teacher is proposing 5 exercises in the form of programming puzzles, and your role is to rank these puzzles from the most appropriate for the course to the least appropriate.
+
+A Python programming puzzle is defined by two functions, the *puzzle* f(...) and the *solution* g(...). f defines an algorithmic challenge, and g solves this challenge. g is a solution to f if and only if `f(g()) == True`. There can be additional optional keyword arguments to both f and g; these arguments, if they exist, are the same for both functions.
+
+Format of the description, puzzle and solution:
+```python
+Problem description: ...
+
+def f(solution, args=...) -> bool:
+    # Python code to test the solution returned by g.
+    # This function behaves like a unit test and must return True if the solution is correct, False otherwise.
+
+def g(args=...) -> solution:
+    # Python code to generate a solution for the problem.
+    # The solution should generalize to all possible args.
+    return solution
+```
+
+You are ranking puzzles for a second year undergraduate Python course. Puzzles and their solutions should be ranked highly if they provide important opportunities for learning for students of that level (both by searching for the solution and looking at the provided solution, so you should look at the quality of both f and g). Clarity is important. You will be presented with 5 puzzles and their solutions, with random 4-letter ids. You should respond by giving your ranking as a list of ids, from the most relevant to least relevant. The ids should be inside double quotes, like so: Answer: ["aaaa", "bbbb", ...]
+
+{examples}
+
+Answer:"""
+
+
+five_fold_ranking_prompt_scrambled_cot = """I am teaching a second-year undergraduate programming course in Python. I am proposing 5 exercises in the form of programming puzzles, and your role is to rank these puzzles from the most appropriate for the course to the least appropriate.
+
+A Python programming puzzle is defined by two functions, the *puzzle* f(...) and the *solution* g(...). f defines an algorithmic challenge, and g solves this challenge. g is a solution to f if and only if `f(g()) == True`. There can be additional optional keyword arguments to both f and g; these arguments, if they exist, are the same for both functions.
+
+Format of the description, puzzle and solution:
+```python
+Problem description: ...
+
+def f(solution, args=...) -> bool:
+    # Python code to test the solution returned by g.
+    # This function behaves like a unit test and must return True if the solution is correct, False otherwise.
+
+def g(args=...) -> solution:
+    # Python code to generate a solution for the problem.
+    # The solution should generalize to all possible args.
+    return solution
+```
+
+You are ranking puzzles for a second year undergraduate Python course. The evaluation criteria for good puzzles are:
+
+* Clarity (how easy is it to understand what needs to be done?);
+* Opportunity for learning (how likely is it that a student will learn by trying to solve the puzzle and looking at its solution?);
+* Difficulty appropriate for the course (is the puzzle too easy. tooo hard, or just right?);
+* Engagement (is the puzzle fun or boring?);
+
+You will be presented with 5 puzzles and their solutions, with random 4-letter ids. You should respond by giving your ranking as a list of ids, from the most relevant to least relevant. The ids should be inside double quotes. You should provide a justification for your ranking, and then the ranking.
+
+Example: if the puzzle ids are "aaaa", "bbbb", "cccc". "dddd" and "eeee" you should respond like so: Reasoning: ... 
+Answer: ["cccc", "bbbb", ...]
+
+The puzzles are:
+
+{examples}
+
+Reasoning:
+"""
+
+
