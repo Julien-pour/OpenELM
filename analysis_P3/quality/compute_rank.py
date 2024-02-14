9b2575efc6544e1474214aacac4ca972a9f301fb
Julien Pourcel <j.pourcel31830@gmail.com>
2024-02-14 19:38:16 +0100

    wip add openchat

--- analysis_P3/quality/compute_rank.py
+++ analysis_P3/quality/compute_rank.py
@@ -114,32 +114,31 @@ class Rank_puzzle(ABC):
         else:
             raise ValueError(f"Invalid ranking mode: {self.mode_rank}")
     
+# HF model
 
-# evaluate with GAIR/autoj-13b-GPTQ-4bits 
-        
-class Auto_j_Rank(Rank_puzzle):
-    def __init__(self, puzzle_dict,mode_rank="pairwise",prompt_instruction=None,exllama2=True) -> None:
+class HF_Rank(Rank_puzzle):
+    def __init__(self, puzzle_dict,mode_rank="pairwise",prompt_instruction=None,exllama2=True,model_id=None,revision="main") -> None:
         self.exllama2 = exllama2
+        self.model_id = model_id
+        self.revision = revision
         super().__init__(puzzle_dict=puzzle_dict,mode_rank=mode_rank,prompt_instruction=prompt_instruction)
-        
+
     def init_model(self):
         from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
-    
-
-        path_model="GAIR/autoj-13b-GPTQ-4bits"
-        self.tokenizer = AutoTokenizer.from_pretrained(path_model)
+        path_model=self.model_id
+        self.tokenizer = AutoTokenizer.from_pretrained(path_model,revision = self.revision)
         if self.exllama2:
             gptq_config = GPTQConfig(bits=4, exllama_config={"version":2})
-            self.model = AutoModelForCausalLM.from_pretrained(path_model,device_map="auto",quantization_config=gptq_config)
+            self.model = AutoModelForCausalLM.from_pretrained(path_model,device_map="auto",quantization_config=gptq_config,revision = self.revision)
         else:
-            self.model = AutoModelForCausalLM.from_pretrained(path_model,device_map="auto")
+            self.model = AutoModelForCausalLM.from_pretrained(path_model,device_map="auto",revision = self.revision)
         self.model=torch.compile(self.model)
-        
+
     def generate(self,text):
         with torch.no_grad():
             time_s = time.time()
             inputs = self.tokenizer(text, return_tensors="pt").to("cuda")
-            out_tok = self.model.generate(**inputs, max_length=2048, temperature=0.0, do_sample=False, top_p=1.0)
+            out_tok = self.model.generate(**inputs, max_length=2048, do_sample=False, top_p=1.0)
             out = self.tokenizer.decode(out_tok[0], skip_special_tokens=True)
             time_e = time.time()
             speed = len(out_tok[0])/((time_e-time_s)) # result in tokens per second
@@ -152,7 +151,15 @@ class Auto_j_Rank(Rank_puzzle):
                 self.speed_inference = self.speed_inference
                 self.speed_inference = (speed * alpha) + (self.speed_inference * (1-alpha))
         return out
+    
+# evaluate with GAIR/autoj-13b-GPTQ-4bits 
 
+class Auto_j_Rank(HF_Rank):
+    def __init__(self, puzzle_dict,mode_rank="pairwise",prompt_instruction=None,exllama2=True,model_id="GAIR/autoj-13b-GPTQ-4bits") -> None:
+        self.exllama2 = exllama2
+        super().__init__(model_id,puzzle_dict=puzzle_dict,mode_rank=mode_rank,prompt_instruction=prompt_instruction,model_id=model_id)
+        
+        
     def pairwise_ranking(self,puzzle1: str,puzzle2: str) -> str:
         """return the winner (puzzle1 or puzzle2)"""
         query = self.prompt_instruction
@@ -173,3 +180,55 @@ class Auto_j_Rank(Rank_puzzle):
                     protocol = "single") # for single response evaluation 
         out = self.generate(input_single)
         return extract_single_rating_autoj(out)
+    
+# TheBloke/openchat-3.5-1210-GPTQ
+prompt_openchat = """GPT4 Correct User: {instruct}<|end_of_turn|>GPT4 Correct Assistant: Hi<|end_of_turn|>GPT4 Correct User: How are you today?<|end_of_turn|>GPT4 Correct Assistant:"""
+instruction_openchat="""###Task Description:
+An instruction (might include an Input inside it), a response to evaluate, a reference answer that gets a score of 5, and a score rubric representing a evaluation criteria are given.
+1. Write a detailed feedback that assess the quality of the response strictly based on the given score rubric, not evaluating in general.
+2. After writing a feedback, write a score that is an integer between 1 and 5. You should refer to the score rubric.
+3. The output format should look as follows: "Feedback: (write a feedback for criteria) [RESULT] (an integer number between 1 and 5)"
+4. Please do not generate any other opening, closing, and explanations.
+
+###The instruction to evaluate:
+{orig_instruction}
+
+###Response to evaluate:
+{orig_response}
+
+###Reference Answer (Score 5):
+{orig_reference_answer}
+
+###Score Rubrics:
+[{orig_criteria}]
+Score 1: {orig_score1_description}
+Score 2: {orig_score2_description}
+Score 3: {orig_score3_description}
+Score 4: {orig_score4_description}
+Score 5: {orig_score5_description}
+
+###Feedback:
+"""
+to_rem="""
+###Reference Answer (Score 5):
+{orig_reference_answer}
+"""
+instruction_openchat_wo_reference = instruction_openchat.replace(to_rem,"")
+# TODO: define description
+
+class Open_chat(HF_Rank):
+    def __init__(self, puzzle_dict,mode_rank="absolute",prompt_instruction=None,exllama2=True,model_id="TheBloke/openchat-3.5-1210-GPTQ",revision="gptq-4bit-32g-actorder_True") -> None:
+        self.exllama2 = exllama2
+        super().__init__(model_id,puzzle_dict=puzzle_dict,mode_rank=mode_rank,prompt_instruction=prompt_instruction,model_id=model_id,revision=revision)
+    
+    def absolute_grade(self,puzzle):
+        """return the absolute_grade float between 0 and 5"""
+        query = self.prompt_instruction
+        resp1 = puzzle
+        instruct = instruction_openchat.format(orig_instruction=query,orig_response=resp1,orig_reference_answer="...",orig_criteria="...",
+                                    orig_score1_description="...",orig_score2_description="...",orig_score3_description="...",
+                                    orig_score4_description="...",orig_score5_description="...")
+        input_single = prompt_openchat.format(instruct=instruct)
+        out = self.generate(input_single)
+        raise NotImplementedError # need to extract the score
+        return  
